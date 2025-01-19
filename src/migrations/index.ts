df13efd1d00c7f42c6f505102b571621add73957
Eric Wätke <eric.waetke@fh-potsdam.de>
2025-01-19 20:21:46 +0100

    feat: Added Migrations

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,5 +1,6 @@
 import * as migration_20250119_115407 from './20250119_115407';
 import * as migration_20250119_123550 from './20250119_123550';
+import * as migration_20250119_192135 from './20250119_192135';
 
 export const migrations = [
   {
@@ -10,6 +11,11 @@ export const migrations = [
   {
     up: migration_20250119_123550.up,
     down: migration_20250119_123550.down,
-    name: '20250119_123550'
+    name: '20250119_123550',
+  },
+  {
+    up: migration_20250119_192135.up,
+    down: migration_20250119_192135.down,
+    name: '20250119_192135'
   },
 ];
